1d839b4c4546d0ee90965360aae8cc757fdca579
BrieanneH <howard.brieanne@gmail.com>
2020-03-29 03:39:25 -0500

    editing pages

---
+++ employee-directory-react/src/pages/searchEmployee.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import container from"../compnents/container";
+import employeeSearch from "..components/employeeSearch";
+
+//within an extends
+state ={
+    users: [],
+    store: []
+};
+
+//if component mounts then get list and update the this.state.person
+componentDidMount(){
+    
+};
+
+
+
+
+export default searchEmployee;
